67365e6aecd87299654ffe57fa3f0147ad14b1de
Phong Lam <phong@mystenlabs.com>
2024-04-04 22:48:35 -0700

    add redirect

--- apps/explorer/src/pages/reference/index.tsx
+++ apps/explorer/src/pages/reference/index.tsx
@@ -1,7 +1,19 @@
-import {CheckboxRedirectReference} from "~/components/CheckboxRedirectReference";
+import {CheckboxRedirectReference, RedirectExplorer, useReference} from "~/components/CheckboxRedirectReference";
+import {RadioGroup, RadioGroupItem} from "@mysten/ui";
 
 export function Reference() {
+    const {
+        reference,
+        setReference,
+    } = useReference();
+
     return (
-        <CheckboxRedirectReference />
+        <div>
+            <RadioGroup aria-label="redirect-preferences" value={reference} onValueChange={(reference) => setReference(reference as RedirectExplorer)}>
+                <RadioGroupItem value={RedirectExplorer.SUISCAN} label={RedirectExplorer.SUISCAN} />
+                <RadioGroupItem value={RedirectExplorer.SUIVISION} label={RedirectExplorer.SUIVISION} />
+            </RadioGroup>
+            <CheckboxRedirectReference />
+        </div>
     );
 }
