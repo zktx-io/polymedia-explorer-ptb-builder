37865283c90fe508bd8fe37fd958a097b4764ae0
juzybits <96890594+juzybits@users.noreply.github.com>
2024-06-14 21:49:53 +0000

    remove prettier

--- apps/core/src/hooks/useAppsBackend.ts
+++ apps/core/src/hooks/useAppsBackend.ts
@@ -1,6 +1,5 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
-/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-return */
 
 import { useCallback } from "react";
 
