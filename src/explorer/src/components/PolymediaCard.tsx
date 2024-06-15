d966b71b0fd69624c1cc580f2246b12398d15924
juzybits <96890594+juzybits@users.noreply.github.com>
2024-06-15 11:49:00 +0000

    add links to polymedia card

--- src/explorer/src/components/PolymediaCard.tsx
+++ src/explorer/src/components/PolymediaCard.tsx
@@ -6,21 +6,40 @@ export function PolymediaCard() {
 	return (
 		<Card bg="white/80" spacing="lg" height="full">
 			<div className="flex h-full flex-col gap-4 overflow-hidden">
+
 				<div className="md:text-heading4 text-heading6 text-steel-darker font-semibold">
 					Polymedia Explorer
 				</div>
+
 				<div className="break-words text-pBody font-semibold text-steel-dark">
 					The Sui Explorer, maintained by&nbsp;
 					<ButtonOrLink href="https://x.com/juzybits" className="break-words text-pBody font-semibold text-steel-dark">
 						<span className="text-hero-dark font-semibold">@juzybits</span>
 					</ButtonOrLink>
 				</div>
-				<ButtonOrLink href="https://github.com/juzybits/polymedia-explorer" className="break-words text-pBody font-semibold text-steel-dark">
-					Source code: <span className="text-hero-dark font-semibold">github.com</span>
-				</ButtonOrLink>
-				<ButtonOrLink href="https://app.turbos.finance/#/trade?input=0x2::sui::SUI&output=0x30a644c3485ee9b604f52165668895092191fcaf5489a846afa7fc11cdb9b24a::spam::SPAM" className="break-words text-pBody font-semibold text-steel-dark">
-					Support this project: <span className="text-hero-dark font-semibold">buy SPAM</span>
-				</ButtonOrLink>
+
+				<div className="break-words text-pBody font-semibold text-steel-dark">
+					<ButtonOrLink href="https://github.com/juzybits/polymedia-explorer">
+						<span className="text-hero-dark font-semibold">🛠️ SOURCE CODE</span>
+					</ButtonOrLink>
+				</div>
+
+				<div className="break-words text-pBody font-semibold text-steel-dark flex align-middle">
+					<ButtonOrLink href="https://github.com/juzybits/polymedia-explorer">
+						<img alt="polymedia" src="https://assets.polymedia.app/img/all/logo-nomargin-transparent-512x512.webp"
+							className="inline-block h-4"
+						/>
+						&nbsp;
+						<span className="text-hero-dark font-semibold">MORE PROJECTS</span>
+					</ButtonOrLink>
+				</div>
+
+				<div className="break-words text-pBody font-semibold text-steel-dark">
+					<ButtonOrLink href="https://spamsui.com/">
+						<span className="text-hero-dark font-semibold">🕯️ JOIN THE CULT</span>
+					</ButtonOrLink>
+				</div>
+
 			</div>
 		</Card>
 	);
