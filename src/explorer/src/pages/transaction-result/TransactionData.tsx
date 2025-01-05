1baacea4332492212ccf5a0be945988783bda76a
Lim Jet <57783762+daoauth@users.noreply.github.com>
2025-01-06 02:08:22 +0900

    Update TransactionData.tsx

--- src/explorer/src/pages/transaction-result/TransactionData.tsx
+++ src/explorer/src/pages/transaction-result/TransactionData.tsx
@@ -1,13 +1,13 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { useTransactionSummary } from "@mysten/core";
 import {
 	type ProgrammableTransaction,
 	type SuiTransactionBlockResponse,
 } from "@mysten/sui/client";
-import { PTBBuilder } from "@zktx.io/ptb-builder";
+import { PTB_SCHEME, PTBBuilder } from "@zktx.io/ptb-builder";
 import { enqueueSnackbar } from "notistack";
 
 import { TransactionDetailCard } from "./transaction-summary/TransactionDetailCard";
@@ -39,23 +39,51 @@ export function TransactionData({ transaction }: Props) {
 
 	const [network] = useNetwork();
 
+  const [activeTab, setActiveTab] = useState("data");
+  
+  const [ptb, setPTB] = useState<PTB_SCHEME | undefined>(undefined);
+
 	const transactionKindName = transaction.transaction?.data.transaction.kind;
 
 	const isProgrammableTransaction = transactionKindName === "ProgrammableTransaction";
 
 	const programmableTxn = transaction.transaction!.data.transaction as ProgrammableTransaction;
 
+  const handleSavePTB = () => {
+    if (ptb) {
+      const jsonString = JSON.stringify(ptb);
+      const blob = new Blob([jsonString], { type: "application/json" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "ptb_data.ptb";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    } else {
+      enqueueSnackbar("No PTB data available.", { variant: "error" });
+    }
+  };
+
 	return (
 		<div className="flex flex-col gap-3 pl-1 pr-2 md:gap-6">
-        <Tabs size="lg" defaultValue="data">
-          <TabsList>
-            <TabsTrigger value="data">Data</TabsTrigger>
-            {isProgrammableTransaction &&
-              network &&
-              network !== "LOCAL" &&
-              transaction.digest && (
+        <Tabs size="lg" defaultValue="data" onValueChange={(value) => setActiveTab(value)}>
+          <TabsList className="flex justify-between items-center">
+            <div className="flex space-x-2">
+              <TabsTrigger value="data">Data</TabsTrigger>
+              {isProgrammableTransaction && network && network !== "LOCAL" && transaction.digest && (
                 <TabsTrigger value="ptbBuilder">PTB Builder</TabsTrigger>
               )}
+            </div>
+            {activeTab === "ptbBuilder" && (
+              <button
+                onClick={handleSavePTB}
+                className="bg-gray-50 text-black px-2 py-1 text-sm rounded transition-colors duration-300 hover:bg-blue-500 hover:text-white active:scale-95"
+              >
+                Save PTB
+              </button>
+            )}
           </TabsList>
           <TabsContentContainer value="data">
             <section className="flex w-full flex-1 flex-col gap-3 md:gap-6">
@@ -85,6 +113,9 @@ export function TransactionData({ transaction }: Props) {
                   options={{
                     canEdit: false,
                     themeSwitch: true,
+                  }}
+                  update={(data) => {
+                    setPTB(data);
                   }}
                   enqueueToast={(message, options) =>
                     enqueueSnackbar(message, options)
